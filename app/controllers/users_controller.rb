28e8e0bdbb0cb12c539c886c4a3e1ebcde91d463
sukepio <naoto927sukena@gmail.com>
2021-01-09 08:01:34 +0000

    [add] 投稿機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'about' => 'homes#about'
   
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
