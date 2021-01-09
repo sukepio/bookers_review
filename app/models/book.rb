7385cdaeeec204b51ba5e46e25ca0e5f4ba85617
sukepio <naoto927sukena@gmail.com>
2021-01-09 13:52:34 +0000

    [Add] BookCommentsコントローラー・モデル

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   
   belongs_to :user
+  has_many :book_comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 end
