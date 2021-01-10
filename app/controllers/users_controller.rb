7b60c30c576318ecad58e2f67f57b5110c9ac543
sukepio <naoto927sukena@gmail.com>
2021-01-10 00:59:17 +0000

    [add] プロフィール編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path()
   end
 
   def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
