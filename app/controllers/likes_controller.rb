95bb2ee95f186eeeaa7f551eec00c63a66ec00eb
sukepio <naoto927sukena@gmail.com>
2021-01-11 02:55:58 +0000

    [add] いいね非同期

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,14 @@ class LikesController < ApplicationController
   
   
   def create
-    book = Book.find(params[:book_id])
-    like = current_user.likes.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    like = current_user.likes.new(book_id: @book.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    like = current_user.likes.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    like = current_user.likes.find_by(book_id: @book.id)
     like.destroy
-    redirect_to request.referer
   end
 end
